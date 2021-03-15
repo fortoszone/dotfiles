c982ecb5b797745bc8994056c31f8a1895623d32
fortoszone <Fortoszone@gmail.com>
2021-03-15 21:47:23 +0700

    Update: 15-03-2021 @ 21:47:23

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -7,12 +7,12 @@ timestamp() {
 cp ~/.zshrc ~/dotfiles/.zshrc
 cp ~/.fehbg ~/dotfiles/.fehbg
 cp ~/.config/openbox ~/dotfiles/.config/openbox
-cp ~/.config/kitty ~/dotfiles/.config/kitty
-cp ~/.config/dunst/dunstrc ~/dotfiles/.config/dunst/dunstrc
-cp ~/.config/obmenu-generator ~/dotfiles/.config/obmenu-generator
-cp ~/.config/picom/picom.conf ~/dotfiles/.config/picom/picom.conf
-cp ~/.config/rofi ~/dotfiles/.config/rofi
-cp ~/.config/tint2/tint2rc ~/dotfiles/.config/tint2/tint2rc
+cp -R ~/.config/kitty ~/dotfiles/.config/kitty
+cp -R ~/.config/dunst/dunstrc ~/dotfiles/.config/dunst/dunstrc
+cp -R ~/.config/obmenu-generator ~/dotfiles/.config/obmenu-generator
+cp -R ~/.config/picom/picom.conf ~/dotfiles/.config/picom/picom.conf
+cp -R ~/.config/rofi ~/dotfiles/.config/rofi
+cp -R ~/.config/tint2/tint2rc ~/dotfiles/.config/tint2/tint2rc
 
 # pull & push
 if [[ `git status --porcelain` ]]; then
